b0e2b0553bc55b25554f390c8bc34309ce2b3e70
Chunfang Li <cli449@gatech.edu>
2016-05-13 10:17:50 -0700

    BFS implementation

--- Udemy/src/BreadthFirstSearch/App.java
+++ Udemy/src/BreadthFirstSearch/App.java
@@ -1,5 +1,19 @@
 package BreadthFirstSearch;
 
 public class App {
-
+	public static void main(String[] args) {
+		BFS breadthFirstSearch = new BFS();
+		Vertex vertex1 = new Vertex(1);
+		Vertex vertex2 = new Vertex(2);
+		Vertex vertex3 = new Vertex(3);
+		Vertex vertex4 = new Vertex(4);
+		Vertex vertex5 = new Vertex(5);
+		
+		vertex1.addNeighbor(vertex2);
+		vertex1.addNeighbor(vertex4);
+		vertex4.addNeighbor(vertex5);
+		vertex2.addNeighbor(vertex3);
+		
+		breadthFirstSearch.bfs(vertex1);
+	}
 }
