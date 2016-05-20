e87816fbba8dcfb492e74c39d80bc17817badf66
Chunfang Li <cli449@gatech.edu>
2016-05-20 11:48:58 -0700

    shunting yard algorithm

---
+++ Algorithm/src/com/cli/DijkstraInterpreter/App.java
@@ -0,0 +1,9 @@
+package com.cli.DijkstraInterpreter;
+
+public class App {
+	public static void main(String[] args) {
+		Algorithm algorithm = new Algorithm();
+		algorithm.interpretExpression("( ( 7 + 2 ) / ( 6 - 3 ) )");
+		algorithm.result();
+	}
+}
