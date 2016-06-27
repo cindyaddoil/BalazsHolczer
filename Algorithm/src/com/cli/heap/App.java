aab8980b9ae5265fbdc7fba3beb796b269dc12d0
Chunfang Li <cli449@gatech.edu>
2016-06-27 15:03:59 -0700

    continue

--- Algorithm/src/com/cli/heap/App.java
+++ Algorithm/src/com/cli/heap/App.java
@@ -1,5 +1,19 @@
 package com.cli.heap;
 
-public class App {
 
+public class App {
+	public static void main(String[] args) {
+		
+		Heap heap = new Heap(10);
+		
+		heap.insert(10);
+		heap.insert(100);
+		heap.insert(-10);
+		
+		heap.heapsort();
+		
+		System.out.println();
+		System.out.println(heap.getMax());
+		
+	}
 }
---
+++ Algorithm/src/com/cli/ternarysearchtree/App.java
@@ -0,0 +1,10 @@
+package com.cli.ternarysearchtree;
+
+public class App {
+	public static void main(String[] args) {
+		TST tst = new TST();
+		tst.put("apple", 1);
+		tst.put("orange", 2);
+		System.out.println(tst.get("apple"));
+	}
+}
